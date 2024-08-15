a3dc6768d34b2f58f7c1fd5e9faada59b14b91f2
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-08-15 13:48:00 +0530

    added the bg of random user

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ProfileCard from '../Components/ProfileCard';
+import RandomUserBG from '../Asset/RandomUserBG.png'
 export default function Home() {
   return (
-    <div className='h-screen w-screen bg-black flex justify-center items-center'>
+    <div className='h-screen w-screen bg-black flex justify-center items-center ' style={{
+      backgroundImage: `url(${RandomUserBG})`,
+      backgroundSize: 'cover', // Cover the entire div
+      backgroundPosition: 'center', // Center the image
+    }}>
       <ProfileCard />
     </div>
   );
