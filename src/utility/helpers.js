d5f9b2d1041adbeefe1adc8a681a262519d8a064
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-08-15 18:35:57 +0530

    random jokes utils

--- src/utility/helpers.js
+++ src/utility/helpers.js
@@ -16,3 +16,32 @@ export function formatDate(dateString) {
     console.log(data[0]?.cca2)
     return data[0]?.cca2;
   }
+
+  // for random jokes
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  function getRandomDate(start, end) {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+  }
+  
+  function formatDateTime(date) {
+    const timeOptions = { hour: '2-digit', minute: '2-digit', hour12: true };
+    const formattedTime = date.toLocaleTimeString(undefined, timeOptions).toLowerCase();
+    
+    const formattedDate = `${date.getDate()} ${date.toLocaleString('default', { month: 'long' })} ${date.getFullYear()}`;
+    
+    return `${formattedTime} · ${formattedDate}`;
+  }
+  
+  export function generateRandomData() {
+    const v = `${getRandomInt(1, 9)}.${getRandomInt(1, 9)}K`;
+    const l = `${getRandomInt(1, 9)}.${getRandomInt(1, 9)}K`;
+    const re = `${getRandomInt(1, 9)}.${getRandomInt(1, 9)}K`;
+    const c = `${getRandomInt(1, 9)}.${getRandomInt(1, 9)}K`;
+    const s = `${getRandomInt(1, 9)}.${getRandomInt(1, 9)}K`;
+    const d = formatDateTime(getRandomDate(new Date(2022, 0, 1), new Date()));
+  
+    return { v, l, re, d, c,s};
+  }
