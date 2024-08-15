0b7e73b0380a1bd7dc458fa3ff5bab454b23e2ef
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-08-15 10:25:37 +0530

    icons installed and user data added to ui

--- src/Components/ProfileCard.js
+++ src/Components/ProfileCard.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { useState } from 'react';
 import { useEffect } from 'react';
-
+import chai from '../Asset/chai.png';
+import { IoCallOutline } from "react-icons/io5";
+import { SlLocationPin } from "react-icons/sl";
+import { FaArrowLeft } from "react-icons/fa";
+import { IoMdRefresh } from "react-icons/io";
 
 function ProfileCard() {
     const [userData, setUserData] = useState(null);
@@ -14,6 +18,9 @@ function ProfileCard() {
     setUserData(userdata?.data);
     console.log(userdata);
 }
+    const loadChaiCode = () =>{
+        
+    }
     useEffect(() => {
         loadUser();
     },[])
@@ -24,23 +31,93 @@ function ProfileCard() {
         )
     }
   return (
-    <div className='text-black text-opacity-70 w-[358px] bg-[#B6B3F3] h-[610px] border-8  border-white p-4 rounded-xl flex flex-col'>
-            <div className='flex justify-between items-center'>
-                <div>Ar</div>
+    <div className='text-black text-opacity-70 w-[358px] bg-[#B6B3F3] h-[610px] border-8  border-white p-4 rounded-xl flex flex-col items-center gap-5'>
+            
+            <div className='flex justify-between items-center w-full'>
+                <div><FaArrowLeft /></div>
                 <div className='w-[117px] h-[22px] text-center'>
                     <p >Profile Overview</p>
                 </div>
-                <div>re</div>
+                <div><IoMdRefresh/></div>
             </div>
 
             {/* image  */}
-            <div className=' flex justify-center items-center w-full'>
-                <img src={userData?.picture?.medium}  alt='profile pic' className='rounded-full w-[100px] h-[100px] pt-[189px]'/>
+            <div className='relative w-[100px] h-[100px] '>
+                <img src={userData?.picture?.medium}  alt='profile pic' className='rounded-full w-[100px] h-[100px] shadow-md'/>
+                <div className='absolute -right-3 -top-2 bg-black w-[22px] h-[15px] rounded-xl text-white'>
+                    <p className='text-[10px] text-center'>{userData?.name?.title}</p>
+                </div>
             </div>
 
             {/* name  */}
-            <div className=' w-[137px] h-[30px] '>
-                <p className=''>{userData?.name?.first + ' ' + userData?.name?.last} </p>
+            <div >
+                <p className='text-2xl font-[400]'>{userData?.name?.first + ' ' + userData?.name?.last} </p>
+            </div> 
+            {/* username  */}
+            <div >
+                <p className='text-2xl font-[400]'>{userData?.login?.username} </p>
+            </div> 
+
+            {/* button */}
+            <div className='flex gap-1 items-center'>
+                {/* location */}
+                <div className='bg-black w-[22px] h-[22px] rounded-full text-white flex justify-center items-center'>
+                    <SlLocationPin className='align-middle'/>
+                </div>
+                <div >
+                    <p className='w-[37px] h-[12px] text-[9px] text-center font-[500]'>Location</p>
+                </div>
+
+                {/* callme button  */}
+                <div className='bg-black w-[22px] h-[22px] rounded-full text-white ml-3 flex justify-center items-center'>
+                    <IoCallOutline/>
+                </div>
+                <div >
+                    <p className='w-[37px] h-[12px] text-[9px] text-center font-[500]'>call me</p>
+                </div>
+            </div>
+
+            {/* city & Nationality */}
+            <div className='flex items-center  w-full'>
+                <div className='flex flex-col gap-1 w-[55%]'>
+                    <div className='text-[9px] font-[400]'>City</div>
+                    <div className='text-[18px] font-[400] text-opacity-70 capitalize'>{userData?.location?.city}</div>
+                </div>
+                <div className='flex flex-col gap-1 '>
+                    <div className='text-[9px] font-[400]'>Nationality</div>
+                    <div className='text-[18px] font-[400] text-opacity-70 capitalize'>{userData?.location?.country}</div>
+                </div>
+            </div>
+            <div className='flex items-center  w-full'>
+                <div className='flex flex-col gap-1 w-[55%]'>
+                    <div className='text-[9px] font-[400]'>Date of birth</div>
+                    <div className='text-[18px] font-[400] text-opacity-70 capitalize'>{userData?.dob?.date.substring(0,10)}</div>
+                </div>
+                <div className='flex flex-col gap-1 '>
+                    <div className='text-[9px] font-[400]'>Phone No.</div>
+                    <div className='text-[18px] font-[400] text-opacity-70 capitalize'>{userData?.phone}</div>
+                </div>
+            </div>
+
+            <div className='flex items-center  w-full'>
+                <div className='flex flex-col gap-1 w-[55%]'>
+                    <div className='text-[9px] font-[400]'>Time Zone</div>
+                    <div className='text-[18px] font-[400] text-opacity-70 capitalize'>{userData?.location?.timezone?.offset}</div>
+                </div>
+                <div className='flex flex-col gap-1 '>
+                    <div className='text-[9px] font-[400]'>Registered Since</div>
+                    <div className='text-[18px] font-[400] text-opacity-70 capitalize'>{userData?.location?.country}</div>
+                </div>
+            </div>
+
+            {/* footer  */}
+            <div className='w-full flex justify-end items-end text-white'>
+                <div className='mr-7 w-[61px] '>
+                    <p className='text-[8px] font-[700]'>chai aur code</p>
+                </div>
+                <div className='ml-6 w-[60px] h-[62px] rounded-xl bg-black' onClick={loadChaiCode}>
+                    <img src={chai} alt='chaicode' className='rounded-xl'/>
+                </div>
             </div>
     </div>
   )
